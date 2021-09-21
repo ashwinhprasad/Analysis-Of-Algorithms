49dbfa5f7278d3bfcf08ce478f1554c1a28c0245
ashwinhprasad <ashwinprasad202@gmail.com>
2021-09-21 22:05:06 +0530

    chalkboard

---
+++ chalkboard/program.cpp
@@ -0,0 +1,32 @@
+/*
+Alice and Bob take turns playing a game, with Alice starting first.
+Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+
+* Choosing any x with 0 < x < n and n % x == 0.
+
+* Replacing the number n on the chalkboard with n - x.
+
+Also, if a player cannot make a move, they lose the game.
+Return true if and only if Alice wins the game, assuming both players play optimally.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n,flag=0;
+    cin>>n;
+    for(int i=n-1;i>0;i--){
+        if(n%i == 0){
+            n=n-i;
+            i=n-1;
+            flag++;
+        }
+    }
+    if(flag%2==0){
+        cout<<"Alice won"<<endl;
+    } else {
+        cout<<"Bob won"<<endl;
+    }
+}
