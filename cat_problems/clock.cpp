b1cdc3fe28fe21991d5d695eaf73e0c7fd7e57b1
ashwinhprasad <ashwinprasad202@gmail.com>
2021-09-27 12:50:51 +0530

    clock optimization

--- cat_problems/clock.cpp
+++ cat_problems/clock.cpp
@@ -12,20 +12,14 @@ int main(){
         curr = a[i];
         if(abs(curr - init) <= 13){
             dist = abs(curr - init);
-            cout<<"from "<<init<<" to "<<curr<<" distance: "<<dist<<endl;
-            init = curr;
-            time += dist + 1;
         } else if (curr - init > 0){
             dist = abs(init - 'a') + 1 + abs('z' - curr);
-            cout<<"from "<<init<<" to "<<curr<<" distance: "<<dist<<endl;
-            init = curr;
-            time += dist + 1;
         } else {
             dist = abs('z' - init) + abs('a' - curr) + 1;
-            cout<<"from "<<init<<" to "<<curr<<" distance: "<<dist<<endl;
-            init = curr;
-            time += dist + 1;
         }
+        cout<<"from "<<init<<" to "<<curr<<" distance: "<<dist<<endl;
+        init = curr;
+        time += dist + 1;
     }
     cout<<time<<endl;
     return 0;
