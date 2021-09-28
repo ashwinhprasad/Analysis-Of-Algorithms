b4fa5e528ffc6fcb1d1ae1c2e2c9e0a900dcb0f5
ashwinhprasad <ashwinprasad202@gmail.com>
2021-09-28 16:25:36 +0530

    long pal

--- Longest_Palindrome/program.cpp
+++ Longest_Palindrome/program.cpp
@@ -7,7 +7,7 @@ int main(){
     scanf("%s",a);
     for(int i=0;i<strlen(a);i++){
         int count=0,flag=0;
-        for(int j=0;j<strlen(a);j++){
+        for(int j=0;j<strlen(b);j++){
             if(b[j] == a[i]){
                 flag = 1;
             }
